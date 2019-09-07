5f49af3bcbc2186afc90709eb5a756f313e9e880
Lwamtekie <ltekie87@yahoo.com>
2019-09-07 13:54:43 -0500

    wip

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime purchaseDate = DateTime.Now;
+
+            string lastName = "Smith";
+
+            var firstName = "Bill";
+
+            Console.WriteLine($"{firstName} {lastName} visited on {purchaseDate}");
+            Console.ReadLine();
+            string[] products = new string[] { "Motorcycle", "Sofa", "Sandals", "Omega Watch", "iPhone" };
+            foreach (var product in products)
+
+            {
+                Console.WriteLine(product);
+            }
+
+            foreach (string product in products)
+            {
+                if (product.Length > 5)
+                {
+                    Console.WriteLine(product);
+                }
+            }
+
+
+
+
+        }
+    }
+}
